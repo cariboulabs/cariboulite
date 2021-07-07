5ea6a860ebcc01514530dd2f71058d330bfda56d
meexmachina <meexmachina@gmail.com>
2021-07-07 16:04:37 +0300

    added fifos - work in progress

--- firmware/lvds_rx.v
+++ firmware/lvds_rx.v
@@ -38,38 +38,47 @@ module lvds_rx
     // Main Process
     always @(negedge i_ddr_clk)
     begin
-        case (r_state_if)
-            state_idle: begin
-                if (i_ddr_data == 2'b10 ) begin
-                    r_state_if <= state_i_phase;
-                    r_data <= 0; // may be redundant as we do a full loop
-                end
-                r_phase_count <= 3'b111;
-                r_push <= 1'b0;
-            end
-
-            state_i_phase: begin
-                if (r_phase_count == 3'b000) begin
+        if (i_reset) begin
+            r_state_if = state_idle;
+            r_push = 1'b0;
+            r_phase_count = 3'b111;
+            r_data = 0;
+        end else begin
+            case (r_state_if)
+                state_idle: begin
+                    if (i_ddr_data == 2'b10 ) begin
+                        r_state_if <= state_i_phase;
+                        r_data <= 0; // may be redundant as we do a full loop
+                    end
                     r_phase_count <= 3'b111;
-                    r_state_if <= state_q_phase;
-                    r_data <= {r_data[29:0], 2'b00};
-                end else begin
-                    r_phase_count <= r_phase_count - 1;
+                    r_push <= 1'b0;
+                end
+
+                state_i_phase: begin
+                    if (r_phase_count == 3'b000) begin
+                        r_phase_count <= 3'b111;
+                        r_state_if <= state_q_phase;
+                        r_data <= {r_data[29:0], 2'b00};
+                    end else begin
+                        r_phase_count <= r_phase_count - 1;
+                        r_data <= {r_data[29:0], i_ddr_data};
+                    end
+                end
+
+                state_q_phase: begin
+                    if (r_phase_count == 3'b001) begin
+                        r_push <= ~i_fifo_full;
+                        r_phase_count <= 3'b000;
+                        r_state_if <= state_idle;
+                    end else begin
+                        r_phase_count <= r_phase_count - 1;
+                    end
                     r_data <= {r_data[29:0], i_ddr_data};
                 end
-            end
+            endcase
+        end
 
-            state_q_phase: begin
-                if (r_phase_count == 3'b001) begin
-                    r_push <= ~i_fifo_full;
-                    r_phase_count <= 3'b000;
-                    r_state_if <= state_idle;
-                end else begin
-                    r_phase_count <= r_phase_count - 1;
-                end
-                r_data <= {r_data[29:0], i_ddr_data};
-            end
-        endcase
+
     end
 
 endmodule // smi_ctrl
