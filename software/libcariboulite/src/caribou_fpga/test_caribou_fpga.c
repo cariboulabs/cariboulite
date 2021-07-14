ffc110b6654a33d2fc0155a83f35a7d5e1da8690
meexmachina <meexmachina@gmail.com>
2021-07-14 13:58:21 +0300

    fpga control testing

--- software/libcariboulite/src/caribou_fpga/test_caribou_fpga.c
+++ software/libcariboulite/src/caribou_fpga/test_caribou_fpga.c
@@ -41,36 +41,103 @@ int main ()
     // init fpga comm
     caribou_fpga_init(&dev, &io_spi_dev);
 
+    // get versions
+    caribou_fpga_versions_st versions = {0};
+    caribou_fpga_get_versions (&dev, &versions);
+    printf("VERSIONS: sys: 0x%02X, manu: 0x%02X, sys_ctrl: 0x%02X, io_ctrl: 0x%02X, smi_ctrl: 0x%02X\n", 
+                            versions.sys_ver,
+                            versions.sys_manu_id,
+                            versions.sys_ctrl_mod_ver,
+                            versions.io_ctrl_mod_ver,
+                            versions.smi_ctrl_mod_ver);
+
+    // get errors
+    uint8_t err_map = 0;
+    caribou_fpga_get_errors (&dev, &err_map);
+    printf("ERRORS: 0x%02X\n", err_map);
+
+    // io control mode
+    uint8_t debug_mode = 0;
+    caribou_fpga_io_ctrl_rfm_en rfmode = 0;
+    caribou_fpga_get_io_ctrl_mode (&dev, &debug_mode, &rfmode);
+    printf("IO_CTRL MODE: debug = %d, rfm = %d (should be %d)\n", debug_mode, rfmode);
+    
+    // io_ctrl_dig
+    int ldo = 0;
+    int led0 = 0;
+    int led1 = 0;
+    int btn = 0;
+    int cfg = 0;
+    caribou_fpga_get_io_ctrl_dig (&dev, &ldo, &led0, &led1, &btn, &cfg);
+    printf("IO_CTRL: ldo: %d, led0: %d, led1: %d, btn: %d, cfg: 0x%02X\n", ldo, led0, led1, btn, cfg);
+
+    // pmod dir
+    uint8_t dir = 0;
+    caribou_fpga_get_io_ctrl_pmod_dir (&dev, &dir);
+    printf("PMOD_DIR: dir = 0x%02X\n", dir);
+
+    // pmod val
+    uint8_t val = 0;
+    caribou_fpga_get_io_ctrl_pmod_val (&dev, &val);
+    printf("PMOD_VAL: val = 0x%02X\n", val);
+
+    // rf state
+    caribou_fpga_rf_pin_st pins = {0};
+    caribou_fpga_get_io_ctrl_rf_state (&dev, &pins);
+    printf("RF_PIN_STATE: val = 0x%02X\n", pins);
+
     printf("MODE = caribou_fpga_io_ctrl_rfm_low_power\npress enter\n");
     caribou_fpga_set_io_ctrl_mode (&dev, 0, caribou_fpga_io_ctrl_rfm_low_power);
+    caribou_fpga_get_io_ctrl_mode (&dev, &debug_mode, &rfmode);
+    printf("IO_CTRL MODE: debug = %d, rfm = %d (should be %d)\n", debug_mode, rfmode, caribou_fpga_io_ctrl_rfm_low_power);
+    printf("RF_PIN_STATE: val = 0x%02X\n", pins);
 
     getchar();
 
     printf("MODE = caribou_fpga_io_ctrl_rfm_bypass\npress enter\n");
     caribou_fpga_set_io_ctrl_mode (&dev, 0, caribou_fpga_io_ctrl_rfm_bypass);
+    caribou_fpga_get_io_ctrl_mode (&dev, &debug_mode, &rfmode);
+    printf("IO_CTRL MODE: debug = %d, rfm = %d (should be %d)\n", debug_mode, rfmode, caribou_fpga_io_ctrl_rfm_bypass);
+    printf("RF_PIN_STATE: val = 0x%02X\n", pins);
 
     getchar();
 
     printf("MODE = caribou_fpga_io_ctrl_rfm_rx_lowpass\npress enter\n");
     caribou_fpga_set_io_ctrl_mode (&dev, 0, caribou_fpga_io_ctrl_rfm_rx_lowpass);
+    caribou_fpga_get_io_ctrl_mode (&dev, &debug_mode, &rfmode);
+    printf("IO_CTRL MODE: debug = %d, rfm = %d (should be %d)\n", debug_mode, rfmode, caribou_fpga_io_ctrl_rfm_rx_lowpass);
+    printf("RF_PIN_STATE: val = 0x%02X\n", pins);
 
     getchar();
 
     printf("MODE = caribou_fpga_io_ctrl_rfm_rx_hipass\npress enter\n");
     caribou_fpga_set_io_ctrl_mode (&dev, 0, caribou_fpga_io_ctrl_rfm_rx_hipass);
+    caribou_fpga_get_io_ctrl_mode (&dev, &debug_mode, &rfmode);
+    printf("IO_CTRL MODE: debug = %d, rfm = %d (should be %d)\n", debug_mode, rfmode, caribou_fpga_io_ctrl_rfm_rx_hipass);
+    printf("RF_PIN_STATE: val = 0x%02X\n", pins);
 
     getchar();
 
     printf("MODE = caribou_fpga_io_ctrl_rfm_tx_lowpass\npress enter\n");
     caribou_fpga_set_io_ctrl_mode (&dev, 0, caribou_fpga_io_ctrl_rfm_tx_lowpass);
+    caribou_fpga_get_io_ctrl_mode (&dev, &debug_mode, &rfmode);
+    printf("IO_CTRL MODE: debug = %d, rfm = %d (should be %d)\n", debug_mode, rfmode, caribou_fpga_io_ctrl_rfm_tx_lowpass);
+    printf("RF_PIN_STATE: val = 0x%02X\n", pins);
 
     getchar();
 
     printf("MODE = caribou_fpga_io_ctrl_rfm_tx_hipass\npress enter\n");
     caribou_fpga_set_io_ctrl_mode (&dev, 0, caribou_fpga_io_ctrl_rfm_tx_hipass);
+    caribou_fpga_get_io_ctrl_mode (&dev, &debug_mode, &rfmode);
+    printf("IO_CTRL MODE: debug = %d, rfm = %d (should be %d)\n", debug_mode, rfmode, caribou_fpga_io_ctrl_rfm_tx_hipass);
+    printf("RF_PIN_STATE: val = 0x%02X\n", pins);
 
     getchar();
     caribou_fpga_set_io_ctrl_mode (&dev, 0, caribou_fpga_io_ctrl_rfm_low_power);
+    caribou_fpga_get_io_ctrl_mode (&dev, &debug_mode, &rfmode);
+    printf("IO_CTRL MODE: debug = %d, rfm = %d (should be %d)\n", debug_mode, rfmode, caribou_fpga_io_ctrl_rfm_low_power);
+    printf("RF_PIN_STATE: val = 0x%02X\n", pins);
+
     // read out stuff
     /*caribou_fpga_versions_st vers = {0};
     uint8_t err_map = 0;
--- software/libcariboulite/test/fpga_comm_test.c
+++ software/libcariboulite/test/fpga_comm_test.c
@@ -7,7 +7,6 @@ int main()
     printf("fpga api test program!\n");
 
     cariboulite_setup_io ();
-    //cariboulite_configure_fpga ("/home/pi/projects/Caribou/Software/CaribouLite/libcariboulite/src/latticeice40/ex_firmware/leds.bin");
     cariboulite_init_submodules ();
     cariboulite_release_submodules();
     cariboulite_release_io ();
