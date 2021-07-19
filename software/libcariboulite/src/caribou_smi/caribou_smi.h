e279717012ea8c5e236e13f251124784347f2741
meexmachina <meexmachina@gmail.com>
2021-07-19 11:50:30 +0300

    smi control cont.

--- software/libcariboulite/src/caribou_smi/caribou_smi.h
+++ software/libcariboulite/src/caribou_smi/caribou_smi.h
@@ -3,6 +3,7 @@
 
 #include "caribou_smi_defs.h"
 #include "io_utils/io_utils.h"
+#include "io_utils/io_utils_sys_info.h"
 
 typedef struct
 {
@@ -14,13 +15,19 @@ typedef struct
     void *phys;     // Physical address
 } caribou_smi_mem_map_st;
 
-#define REG32(m, x) ((volatile uint32_t *)((uint32_t)(m.virt)+(uint32_t)(x)))
+typedef enum
+{
+    caribou_smi_processor_BCM2835 = 0,
+    caribou_smi_processor_BCM2836 = 1,
+    caribou_smi_processor_BCM2837 = 2,
+    caribou_smi_processor_BCM2711 = 3,
+    caribou_smi_processor_UNKNOWN = 4,
+} caribou_smi_processor_type_en;
 
+#define REG32(m, x) ((volatile uint32_t *)((uint32_t)(m.virt)+(uint32_t)(x)))
 
 typedef struct
 {
-    int initialized;
-
     int data_0_pin;
     int num_data_pins;
     int soe_pin;
@@ -30,18 +37,26 @@ typedef struct
     int read_req_pin;
     int write_req_pin;
 
+    int initialized;
+
+    uint32_t ram_size_mbytes;
+    caribou_smi_processor_type_en processor_type;
+    uint32_t phys_reg_base;
+    uint32_t sys_clock_hz;
+    uint32_t bus_reg_base;
+    uint8_t use_video_core_clock;
     caribou_smi_mem_map_st dma_regs, vc_mem, clk_regs, smi_regs;
 
-    volatile SMI_CS_REG  *smi_cs;
-    volatile SMI_L_REG   *smi_l;
-    volatile SMI_A_REG   *smi_a;
-    volatile SMI_D_REG   *smi_d;
-    volatile SMI_DMC_REG *smi_dmc;
-    volatile SMI_DSR_REG *smi_dsr;
-    volatile SMI_DSW_REG *smi_dsw;
-    volatile SMI_DCS_REG *smi_dcs;
-    volatile SMI_DCA_REG *smi_dca;
-    volatile SMI_DCD_REG *smi_dcd;
+    SMI_CS_REG  *smi_cs;
+    SMI_L_REG   *smi_l;
+    SMI_A_REG   *smi_a;
+    SMI_D_REG   *smi_d;
+    SMI_DMC_REG *smi_dmc;
+    SMI_DSR_REG *smi_dsr;
+    SMI_DSW_REG *smi_dsw;
+    SMI_DCS_REG *smi_dcs;
+    SMI_DCA_REG *smi_dca;
+    SMI_DCD_REG *smi_dcd;
     
 } caribou_smi_st;
 
