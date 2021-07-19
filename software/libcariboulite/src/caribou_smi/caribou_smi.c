e628cfff6a6f439a3868d268ca797312aff7d21e
meexmachina <meexmachina@gmail.com>
2021-07-19 15:06:46 +0300

    smi timing and coding

--- software/libcariboulite/src/caribou_smi/caribou_smi.c
+++ software/libcariboulite/src/caribou_smi/caribou_smi.c
@@ -1,42 +1,283 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
 #include "caribou_smi.h"
+
+//===========================================================================
+// Display bit values in register
+static void caribou_smi_disp_reg_fields(char *regstrs, char *name, uint32_t val)
+{
+    char *p=regstrs, *q, *r=regstrs;
+    uint32_t nbits, v;
+
+    printf("%s %08X", name, val);
+    while ((q = strchr(p, ':')) != 0)
+    {
+        p = q + 1;
+        nbits = 0;
+        while (*p>='0' && *p<='9')
+        {
+            nbits = nbits * 10 + *p++ - '0';
+        }
+        v = val & ((1 << nbits) - 1);
+        val >>= nbits;
+        if (v && *r!='_')
+        {
+            printf(" %.*s=%X", q-r, r, v);
+        }
+        while (*p==',' || *p==' ')
+        {
+            p = r = p + 1;
+        }
+    }
+    printf("\n");
+}
+
+
+//===========================================================================
+// Display SMI registers
+// SMI register names for diagnostic print
+static char *smi_regstrs[] = {
+    "CS","LEN","A","D","DSR0","DSW0","DSR1","DSW1",
+    "DSR2","DSW2","DSR3","DSW3","DMC","DCS","DCA","DCD",""
+};
+
+static void caribou_smi_disp_smi(caribou_smi_st* dev)
+{
+    volatile uint32_t *p=REG32(dev->smi_regs, SMI_CS);
+    int i=0;
+
+    while (smi_regstrs[i][0])
+    {
+        printf("%4s=%08X ", smi_regstrs[i++], *p++);
+        if (i%8==0 || smi_regstrs[i][0]==0)
+            printf("\n");
+    }
+}
+
+
+//=====================================================================
+// Open mailbox interface, return file descriptor
+static int caribou_smi_open_mbox(void)
+{
+    int fd;
+
+    if ((fd = open("/dev/vcio", 0)) < 0)
+    {
+        printf("Error: can't open VC mailbox\n");
+    }
+    return(fd);
+}
+
+//=====================================================================
+// Close mailbox interface
+static void caribou_smi_close_mbox(int fd)
+{
+    if (fd >= 0)
+    {
+        close(fd);
+    }
+}
+
+//=====================================================================
+// Send message to mailbox, return first response int, 0 if error
+static uint32_t caribou_smi_msg_mbox(int fd, VC_MSG *msgp)
+{
+    uint32_t ret=0, i;
+
+    // TODO: consider using memset instead?
+    for (i=msgp->dlen/4; i<=msgp->blen/4; i+=4)
+    {
+        msgp->uints[i++] = 0;
+    }
+
+    msgp->len = (msgp->blen + 6) * 4;
+    msgp->req = 0;
+    if (ioctl(fd, _IOWR(100, 0, void *), msgp) < 0)
+    {
+        printf("VC IOCTL failed\n");
+    }
+    else if ((msgp->req&0x80000000) == 0)
+    {
+        printf("VC IOCTL error\n");
+    }
+    else if (msgp->req == 0x80000001)
+    {
+        printf("VC IOCTL partial error\n");
+    }
+    else
+    {
+        ret = msgp->uints[0];
+    }
+    
+    #if DEBUG
+        caribou_smi_disp_vc_msg(msgp);
+    #endif
+
+    return (ret);
+}
+
+//=====================================================================
+// Get virtual memory segment for peripheral regs or physical mem
+static void* caribou_smi_map_segment(void *addr, int size)
+{
+    int fd;
+    void *mem;
+
+    size = PAGE_ROUNDUP(size);
+    if ((fd = open ("/dev/mem", O_RDWR|O_SYNC|O_CLOEXEC)) < 0)
+    {
+        printf("Error: can't open /dev/mem, run using sudo\n");
+    }
+    
+    mem = mmap(0, size, PROT_WRITE|PROT_READ, MAP_SHARED, fd, (uint32_t)addr);
+    close(fd);
+
+    #if DEBUG
+        printf("Map %p -> %p\n", (void *)addr, mem);
+    #endif
+
+    if (mem == MAP_FAILED)
+    {
+        printf("Error: can't map memory\n");
+    }
+    return(mem);
+}
+
+
+//=====================================================================
+// Free mapped memory
+static void caribou_smi_unmap_segment(void *mem, int size)
+{
+    if (mem) 
+    {
+        munmap(mem, PAGE_ROUNDUP(size));
+    }
+}
+
+//=====================================================================
+// Free memory
+static uint32_t caribou_smi_free_vc_mem(int fd, int h)
+{
+    VC_MSG msg={.tag=0x3000f, .blen=4, .dlen=4, .uints={h}};
+    return (h ? caribou_smi_msg_mbox(fd, &msg) : 0);
+}
+
+
+//=====================================================================
+// Allocate memory on PAGE_SIZE boundary, return handle
+static uint32_t caribou_smi_alloc_vc_mem(int fd, uint32_t size, VC_ALLOC_FLAGS flags)
+{
+    VC_MSG msg={.tag=0x3000c, .blen=12, .dlen=12, .uints={PAGE_ROUNDUP(size), PAGE_SIZE, flags}};
+    return (caribou_smi_msg_mbox(fd, &msg));
+}
+
+//=====================================================================
+// Lock allocated memory, return bus address
+static void *caribou_smi_lock_vc_mem(int fd, int h)
+{
+    VC_MSG msg={.tag=0x3000d, .blen=4, .dlen=4, .uints={h}};
+    return (h ? (void *)caribou_smi_msg_mbox(fd, &msg) : 0);
+}
+
+//=====================================================================
+// Unlock allocated memory
+static uint32_t caribou_smi_unlock_vc_mem(int fd, int h)
+{
+    VC_MSG msg={.tag=0x3000e, .blen=4, .dlen=4, .uints={h}};
+    return (h ? caribou_smi_msg_mbox(fd, &msg) : 0);
+}
+
+//=====================================================================
+// Use mmap to obtain virtual address, given physical
+static void* caribou_smi_map_periph(caribou_smi_st* dev, caribou_smi_mem_map_st *mp, void *phys, int size)
+{
+    mp->phys = phys;
+    mp->size = PAGE_ROUNDUP(size);
+    mp->bus = (void *)((uint32_t)phys - dev->phys_reg_base + dev->bus_reg_base);
+    mp->virt = caribou_smi_map_segment(phys, mp->size);
+    return(mp->virt);
+}
+
+//=====================================================================
+// Free mapped peripheral or memory
+static void caribou_smi_unmap_periph_mem(caribou_smi_st* dev, caribou_smi_mem_map_st *mp)
+{
+    if (mp)
+    {
+        if (mp->fd)
+        {
+            caribou_smi_unmap_segment(mp->virt, mp->size);
+            caribou_smi_unlock_vc_mem(mp->fd, mp->h);
+            caribou_smi_free_vc_mem(mp->fd, mp->h);
+            caribou_smi_close_mbox(mp->fd);
+        }
+        else
+        {
+            caribou_smi_unmap_segment(mp->virt, mp->size);
+        }
+    }
+}
 
 //=====================================================================
 static void caribou_smi_map_devices(caribou_smi_st* dev)
 {
-    map_periph(&dev->dma_regs, (void *)(DMA_BASE + dev->phys_reg_base), PAGE_SIZE);
-    map_periph(&dev->clk_regs, (void *)(CLK_BASE + dev->phys_reg_base), PAGE_SIZE);
-    map_periph(&dev->smi_regs, (void *)(SMI_BASE + dev->phys_reg_base), PAGE_SIZE);
+    caribou_smi_map_periph(dev, &dev->dma_regs, (void *)(DMA_BASE + dev->phys_reg_base), PAGE_SIZE);
+    caribou_smi_map_periph(dev, &dev->clk_regs, (void *)(CLK_BASE + dev->phys_reg_base), PAGE_SIZE);
+    caribou_smi_map_periph(dev, &dev->smi_regs, (void *)(SMI_BASE + dev->phys_reg_base), PAGE_SIZE);
 }
 
 //=====================================================================
 // Free memory segments and exit
 static void caribou_smi_unmap_devices(caribou_smi_st* dev)
 {    
-    unmap_periph_mem(&dev->vc_mem);
-    unmap_periph_mem(&dev->smi_regs);
-    unmap_periph_mem(&dev->dma_regs);
+    caribou_smi_unmap_periph_mem(dev, &dev->vc_mem);
+    caribou_smi_unmap_periph_mem(dev, &dev->smi_regs);
+    caribou_smi_unmap_periph_mem(dev, &dev->dma_regs);
+}
+
+//=====================================================================
+// Display mailbox message
+void caribou_smi_disp_vc_msg(VC_MSG *msgp)
+{
+    int i;
+
+    printf("VC msg len=%X, req=%X, tag=%X, blen=%x, dlen=%x, data ",
+        msgp->len, msgp->req, msgp->tag, msgp->blen, msgp->dlen);
+    for (i=0; i<msgp->blen/4; i++)
+    {
+        printf("%08X ", msgp->uints[i]);
+    }
+    printf("\n");
+}
+
+
+//=====================================================================
+// Allocate uncached memory, get bus & phys addresses
+static void* caribou_smi_map_uncached_mem(caribou_smi_st* dev, caribou_smi_mem_map_st *mp, int size)
+{
+    void *ret;
+    mp->size = PAGE_ROUNDUP(size);
+    mp->fd = caribou_smi_open_mbox();
+    ret = (mp->h = caribou_smi_alloc_vc_mem(mp->fd, mp->size, DMA_MEM_FLAGS)) > 0 &&
+        (mp->bus = caribou_smi_lock_vc_mem(mp->fd, mp->h)) != 0 &&
+        (mp->virt = caribou_smi_map_segment(BUS_PHYS_ADDR(mp->bus), mp->size)) != 0
+        ? mp->virt : 0;
+    printf("VC mem handle %u, phys %p, virt %p\n", mp->h, mp->bus, mp->virt);
+    return ret;
 }
 
 //=====================================================================
 // Initialise SMI, given data width, time step, and setup/hold/strobe counts
-// step: in clock cycles of the specific platform (RPI0-3 or RPI4)
-// setup, strobe, and hold: calculated in number of steps.
-// 
-// Thus, the total sample period (in nanosecs) is given by:
-//    step * ( setup + strobe + hold ) * ns_per_clock_cycle
-//
-// For an RPI4 @ 1500 MHz clock => 0.667 nsec / clock cycle
-// The configuration of: (step, setup, strobe, hold) = (4,  3,  8,  4)
-// yields: 0.667nsec*4*(3+8+4) = 8/3*15 = 40 nanoseconds/sample => 25 MSPS
-static void caribou_smi_init_internal(caribou_smi_st* dev, 
-                                                int width, 
-                                                int step, 
-                                                int setup, 
-                                                int strobe, 
-                                                int hold)
+static void caribou_smi_init_internal(  caribou_smi_st* dev, 
+                                        caribou_smi_transaction_size_bits_en width, 
+                                        caribou_smi_timing_st* timing)
 {
-    int divi = step / 2;
+    int divi = timing->step_size / 2;
 
     dev->smi_cs  = (SMI_CS_REG *) REG32(dev->smi_regs, SMI_CS);
     dev->smi_l   = (SMI_L_REG *)  REG32(dev->smi_regs, SMI_L);
@@ -90,12 +331,12 @@ static void caribou_smi_init_internal(caribou_smi_st* dev,
         dev->smi_cs->seterr = 1;
     }
 
-    dev->smi_dsr0->rsetup = setup;
-    dev->smi_dsw0->wsetup = setup;
-    dev->smi_dsr0->rstrobe = strobe;
-    dev->smi_dsw0->wstrobe = strobe;
-    dev->smi_dsr0->rhold = hold;
-    dev->smi_dsw0->whold = hold;
+    dev->smi_dsr0->rsetup = timing->setup_steps;
+    dev->smi_dsw0->wsetup = timing->setup_steps;
+    dev->smi_dsr0->rstrobe = timing->strobe_steps;
+    dev->smi_dsw0->wstrobe = timing->strobe_steps;
+    dev->smi_dsr0->rhold = timing->hold_steps;
+    dev->smi_dsw0->whold = timing->hold_steps;
     dev->smi_dmc->panicr = 8;
     dev->smi_dmc->panicw = 8;
     dev->smi_dmc->reqr = 4;
@@ -151,6 +392,35 @@ static void caribou_smi_fill_sys_info(caribou_smi_st* dev, io_utils_sys_info_st 
 }
 
 //=====================================================================
+static float caribou_smi_calculate_clocking (caribou_smi_st* dev, float freq_sps, caribou_smi_timing_st* timing)
+{
+    float clock_rate = dev->sys_clock_hz;
+    float cc_in_ns = 1e9 / clock_rate;
+    float sample_time_ns = 1e9 / freq_sps;
+    
+    int cc_budget = (int)(sample_time_ns / cc_in_ns);
+    if (cc_budget < 6) 
+    {
+        printf("The clock cycle budget for the calculation is too small.\n");
+        return -1;
+    }
+    // try to make a 1:2:1 ratio between setup, strobe and hold 
+    timing->step_size = (int)(roundf((float)cc_budget / 4.0f));
+    timing->setup_steps = 1;
+    timing->strobe_steps = 2;
+    timing->hold_steps = 1;
+
+    return 1e9 / (timing->step_size*cc_in_ns*(timing->setup_steps + timing->strobe_steps + timing->hold_steps));
+}
+
+//=====================================================================
+static void caribou_smi_print_timing (float actual_freq, caribou_smi_timing_st* timing)
+{
+    printf("Timing: step: %d, setup: %d, strobe: %d, hold: %d, actual_freq: %.2f MHz\n", 
+                timing->step_size, timing->setup_steps, timing->strobe_steps, timing->hold_steps, actual_freq / 1e6);
+}
+
+//=====================================================================
 int caribou_smi_init(caribou_smi_st* dev)
 {
     // Get the RPI information
@@ -177,9 +447,14 @@ int caribou_smi_init(caribou_smi_st* dev)
         io_utils_set_gpio_mode(dev->addr0_pin + i, io_utils_alt_1);
     }
 
-    init_smi(SMI_NUM_BITS, SMI_TIMING);
+    // Setup a reading rate of 32 MSPS (16 MSPS x2)
+    caribou_smi_timing_st timing = {0};
+    float actual_sps = caribou_smi_calculate_clocking (dev, 32e6, &timing);
+    caribou_smi_print_timing (actual_sps, &timing);
 
-    /*map_uncached_mem(&dev->vc_mem, VC_MEM_SIZE(NSAMPLES+PRE_SAMP));
+    /*caribou_smi_init_internal(dev, caribou_smi_transaction_size_8bits, &timing);
+
+    caribou_smi_map_uncached_mem(&dev->vc_mem, VC_MEM_SIZE(NSAMPLES+PRE_SAMP));
     dev->smi_dmc->dmaen = 1;
     dev->smi_cs->enable = 1;
     dev->smi_cs->clear = 1;
@@ -226,8 +501,8 @@ int caribou_smi_close(caribou_smi_st* dev)
         io_utils_set_gpio_mode(dev->addr0_pin + i, io_utils_alt_gpio_in);
     }
 
-    /*
-    if (dev->smi_regs.virt)
+    
+    /*if (dev->smi_regs.virt)
     {
         *REG32(dev->smi_regs, SMI_CS) = 0;
     }
@@ -320,48 +595,4 @@ int adc_dma_end(void *buff, uint16_t *data, int nsamp)
     return(nsamp);
 }
 
-
-//===========================================================================
-// Display bit values in register
-void caribou_smi_disp_reg_fields(char *regstrs, char *name, uint32_t val)
-{
-    char *p=regstrs, *q, *r=regstrs;
-    uint32_t nbits, v;
-
-    printf("%s %08X", name, val);
-    while ((q = strchr(p, ':')) != 0)
-    {
-        p = q + 1;
-        nbits = 0;
-        while (*p>='0' && *p<='9')
-            nbits = nbits * 10 + *p++ - '0';
-        v = val & ((1 << nbits) - 1);
-        val >>= nbits;
-        if (v && *r!='_')
-            printf(" %.*s=%X", q-r, r, v);
-        while (*p==',' || *p==' ')
-            p = r = p + 1;
-    }
-    printf("\n");
-}
-
-//===========================================================================
-// Display SMI registers
-// SMI register names for diagnostic print
-static char *smi_regstrs[] = {
-    "CS","LEN","A","D","DSR0","DSW0","DSR1","DSW1",
-    "DSR2","DSW2","DSR3","DSW3","DMC","DCS","DCA","DCD",""
-};
-
-void caribou_smi_disp_smi(caribou_smi_st* dev)
-{
-    volatile uint32_t *p=REG32(dev->smi_regs, SMI_CS);
-    int i=0;
-
-    while (smi_regstrs[i][0])
-    {
-        printf("%4s=%08X ", smi_regstrs[i++], *p++);
-        if (i%8==0 || smi_regstrs[i][0]==0)
-            printf("\n");
-    }
-}*/
+*/
