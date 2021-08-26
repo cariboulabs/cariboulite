f6f529050ff82229120d4cfc6ed1167b58041fb8
meexmachina <meexmachina@gmail.com>
2021-08-26 17:02:07 +0300

    smi

--- software/libcariboulite/src/caribou_smi/caribou_smi.c
+++ software/libcariboulite/src/caribou_smi/caribou_smi.c
@@ -6,6 +6,7 @@
 #include "caribou_smi.h"
 #include "bcm2835_smi.h"
 
+#include <string.h>
 #include <fcntl.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -13,15 +14,25 @@
 #include <sys/ioctl.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <sched.h>
+#include <pthread.h>
 
 
+static char *error_strings[] = CARIBOU_SMI_ERROR_STRS;
 
 static void caribou_smi_print_smi_settings(struct smi_settings *settings);
 static void caribou_smi_setup_settings (struct smi_settings *settings);
 static void dump_hex(const void* data, size_t size);
+static void caribou_smi_init_stream(caribou_smi_st* dev, caribou_smi_stream_type_en type, caribou_smi_channel_en ch);
 
 //=========================================================================
-int caribou_smi_init(caribou_smi_st* dev)
+char* caribou_smi_get_error_string(caribou_smi_error_en err)
+{
+    return error_strings[err];
+}
+
+//=========================================================================
+int caribou_smi_init(caribou_smi_st* dev, caribou_smi_error_callback error_cb, void* context)
 {
     char smi_file[] = "/dev/smi";
     struct smi_settings settings = {0};
@@ -67,6 +78,17 @@ int caribou_smi_init(caribou_smi_st* dev)
         close (fd);
         return -1;
     }
+    dev->current_address = caribou_smi_address_idle;
+
+    // initialize streams
+    caribou_smi_init_stream(dev, caribou_smi_stream_type_write, caribou_smi_channel_900);
+    caribou_smi_init_stream(dev, caribou_smi_stream_type_write, caribou_smi_channel_2400);
+    caribou_smi_init_stream(dev, caribou_smi_stream_type_read, caribou_smi_channel_900);
+    caribou_smi_init_stream(dev, caribou_smi_stream_type_read, caribou_smi_channel_2400);
+
+    dev->error_cb = error_cb;
+    dev->cb_context = context;
+    dev->initialized = 1;
 
     return 0;
 }
@@ -74,7 +96,7 @@ int caribou_smi_init(caribou_smi_st* dev)
 //=========================================================================
 int caribou_smi_close (caribou_smi_st* dev)
 {
-    ZF_LOGI("closing caribou_smi");
+    //ZF_LOGI("closing caribou_smi");
     close (dev->filedesc);
     return 0;
 }
@@ -117,6 +139,205 @@ int caribou_smi_timeout_read(caribou_smi_st* dev, int source, char* buffer, int 
     }
 
     return read(dev->filedesc, buffer, size_of_buf);
+}
+
+//=========================================================================
+static int allocate_buffer_vec(uint8_t*** mat, int num_buffers, int buffer_size)
+{
+    (*mat) = (uint8_t**) malloc ( num_buffers * sizeof(uint8_t*) );
+    if ((*mat) == NULL)
+    {
+        ZF_LOGE("buffer vector allocation failed");
+        return -1;
+    }
+
+    memset( (*mat), 0, num_buffers * sizeof(uint8_t*) );
+
+    int failed = 0;
+    int i;
+    for (i = 0; i < num_buffers; i++)
+    {
+        (*mat)[i] = (uint8_t*)calloc( buffer_size, sizeof(uint8_t) );
+        if ((*mat)[i] == NULL)
+        {
+            failed = 1;
+            break;
+        }
+    }
+    if (failed)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            free((*mat)[j]);
+        }
+        free((*mat));
+
+        ZF_LOGE("buffer (%d) allocation failed", i);
+        return -1;
+    }
+
+    return 0;
+}
+
+//=========================================================================
+static void release_buffer_vec(uint8_t** mat, int num_buffers, int buffer_size)
+{
+    if (mat == NULL)
+        return;
+
+    for (int i = 0; i < num_buffers; i ++)
+    {
+        if (mat[i] != NULL) free (mat[i]);
+    }
+
+    free(mat);
+}
+
+//=========================================================================
+void* caribou_smi_thread(void *arg)
+{
+    pthread_t id = pthread_self();
+    caribou_smi_stream_st* st = (caribou_smi_stream_st*)arg;
+    caribou_smi_st* dev = (caribou_smi_st*)st->parent_dev;
+
+    ZF_LOGD("Entered thread id %u", id);
+
+    st->active = 1;
+
+    if (st->data_cb != NULL) st->data_cb(dev->cb_context,
+                                        caribou_smi_stream_start,
+                                        (caribou_smi_channel_en)(st->stream_id>>1),
+                                        0,
+                                        st->current_app_buffer,
+                                        st->batch_length);
+
+    while (st->active)
+    {
+        int ret = caribou_smi_timeout_read(dev, st->addr, st->current_smi_buffer, st->batch_length, 100);
+        if (ret < 0)
+        {
+            if (dev->error_cb) dev->error_cb(dev->cb_context, caribou_smi_error_read_failed, )
+        }
+        usleep(10000);
+    }
+    /*if(pthread_equal(id,tid[0]))
+    {
+        printf("\n First thread processing\n");
+    }
+    else
+    {
+        printf("\n Second thread processing\n");
+    }
+
+    for(i=0; i<(0xFFFFFFFF);i++);
+*/
+
+    if (st->data_cb != NULL) st->data_cb(dev->cb_context,
+                                        caribou_smi_stream_end,
+                                        (caribou_smi_channel_en)(st->stream_id>>1),
+                                        0,
+                                        st->current_app_buffer,
+                                        st->batch_length);
+
+    ZF_LOGD("Exiting thread id %u", id);
+    return NULL;
+}
+
+//=========================================================================
+int caribou_smi_setup_stream(caribou_smi_st* dev,
+                                caribou_smi_stream_type_en type,
+                                caribou_smi_channel_en channel,
+                                int batch_length, int num_buffers,
+                                caribou_smi_data_callback cb)
+{
+    int stream_id = CARIBOU_SMI_GET_STREAM_ID(type, channel);
+    caribou_smi_stream_st* st = &dev->streams[stream_id];
+    if (st->active)
+    {
+        ZF_LOGE("the requested read stream channel (%d) of type (%d) is already active", channel, type);
+        return -1;
+    }
+
+    st->batch_length = batch_length;
+    st->num_of_buffers = num_buffers;
+    st->data_cb = cb;
+
+    // allocate the buffer vector
+    if (allocate_buffer_vec(&st->buffers, st->num_of_buffers, st->batch_length) != 0)
+    {
+        ZF_LOGE("read buffer-vector allocation failed");
+        return -1;
+    }
+
+    st->current_smi_buffer = st->buffers[0];
+    st->current_app_buffer = NULL;
+
+    // create the reading thread
+    st->stream_id = stream_id;
+    int ret = pthread_create(&st->stream_thread, NULL, &caribou_smi_thread, st);
+    if (ret != 0)
+    {
+        ZF_LOGE("read stream thread creation failed");
+        release_buffer_vec(st->buffers, st->num_of_buffers, st->batch_length);
+        st->buffers = NULL;
+        st->active = 0;
+        return -1;
+    }
+
+    while (!st->active) usleep(10000);
+
+    ZF_LOGI("successfully created read stream for channel %s", channel==caribou_smi_channel_900?"900MHz":"2400MHz");
+    return stream_id;
+}
+
+//=========================================================================
+int caribou_smi_destroy_stream(caribou_smi_st* dev, int id)
+{
+    ZF_LOGD("desroying SMI stream %d", id);
+    if (id >= CARIBOU_SMI_MAX_NUM_STREAMS)
+    {
+        ZF_LOGE("wrong parameter id = %d >= %d", id, CARIBOU_SMI_MAX_NUM_STREAMS);
+        return -1;
+    }
+    if (dev->streams[id].active == 0)
+    {
+        ZF_LOGW("stream id = %d is already not active", id);
+        return 0;
+    }
+
+    dev->streams[id].active = 0;
+    pthread_join(dev->streams[id].stream_thread, NULL);
+
+    release_buffer_vec(dev->streams[id].buffers, dev->streams[id].num_of_buffers, dev->streams[id].batch_length);
+    dev->streams[id].buffers = NULL;
+    dev->streams[id].current_smi_buffer = NULL;
+    dev->streams[id].current_app_buffer = NULL;
+
+    ZF_LOGD("sucessfully desroyed SMI stream %d", id);
+    return 0;
+}
+
+//=========================================================================
+static void caribou_smi_init_stream(caribou_smi_st* dev, caribou_smi_stream_type_en type, caribou_smi_channel_en ch)
+{
+    caribou_smi_address_en addr = ((type << 2) | (ch + 1)) << 1;
+    caribou_smi_stream_st* st = &dev->streams[CARIBOU_SMI_GET_STREAM_ID(type, ch)];
+    st->stream_id = CARIBOU_SMI_GET_STREAM_ID(type, ch);
+
+    ZF_LOGD("initializing stream type: %s, ch: %s, addr: %d, stream_id: %d",
+                    type==caribou_smi_stream_type_write?"write":"read", ch==caribou_smi_channel_900?"900MHz":"2400MHz", addr, st->stream_id);
+
+    st->addr = addr;
+    st->batch_length = 1024;
+    st->num_of_buffers = 2;
+    st->data_cb = NULL;
+
+    st->buffers = NULL;
+    st->current_smi_buffer = NULL;
+    st->current_app_buffer = NULL;
+
+    st->active = 0;
+    st->parent_dev = dev;
 }
 
 
