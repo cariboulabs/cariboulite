2eac875bec0f5ed22306b6bd932d4ae769ecf6f3
meexmachina <meexmachina@gmail.com>
2021-09-05 10:21:08 +0300

    starting soapy!! :)

---
+++ software/SoapyCaribouLite/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/projects/cariboulite/software/SoapyCaribouLite
+# Build directory: /home/pi/projects/cariboulite/software/SoapyCaribouLite/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
