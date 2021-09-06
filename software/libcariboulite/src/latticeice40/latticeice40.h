619837226cb456c2ae35c90186842dbc0034ddf2
meexmachina <meexmachina@gmail.com>
2021-09-06 15:57:21 +0300

    system level api progress

--- software/libcariboulite/src/latticeice40/latticeice40.h
+++ software/libcariboulite/src/latticeice40/latticeice40.h
@@ -24,4 +24,11 @@ int latticeice40_init(	latticeice40_st *dev,
 int latticeice40_release(latticeice40_st *dev);
 int latticeice40_configure(latticeice40_st *dev, char *bitfilename);
 
+/*
+ * Hard reset pin toggling function
+    Level: if -1 => a full reset (1=>0=>1) cycle is performed
+		   if 0 / 1 => the pin is reset or set accordingly
+ */
+int latticeice40_hard_reset(latticeice40_st *dev, int level);
+
 #endif // __LATTICEICE40_H__
