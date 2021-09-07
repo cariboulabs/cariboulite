9336eee767bc52a970c27a718815c817bec794f4
meexmachina <meexmachina@gmail.com>
2021-09-07 23:30:02 +0300

    update main program

--- software/libcariboulite/src/cariboulite.c
+++ software/libcariboulite/src/cariboulite.c
@@ -3,5 +3,30 @@
 #define ZF_LOG_TAG "CARIBOULITE Main"
 #include "zf_log/zf_log.h"
 
+#include "cariboulite_setup.h"
+#include "cariboulite_events.h"
 #include "cariboulite.h"
 
+#include <stdio.h>
+#include <signal.h>
+#include <string.h>
+#include <unistd.h>
+
+struct sigaction act;
+
+void sighandler(int signum, siginfo_t *info, void *ptr)
+{
+    printf("Received signal %d\n", signum);
+    printf("Signal originates from process %lu\n",
+        (unsigned long)info->si_pid);
+}
+
+
+int main(int argc, char *argv[])
+{
+    memset(&act, 0, sizeof(act));
+    act.sa_sigaction = sighandler;
+    act.sa_flags = SA_SIGINFO;
+
+    sigaction(SIGTERM, &act, NULL);
+}
