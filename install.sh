16cf4472c6967731da7d4ad4208b507cd50fb55e
David Michaeli <meexmachina@gmail.com>
2023-01-29 11:23:57 +0200

    added install script start

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@
 
 # determine the system type
 
-# update the git repo to include sub-modules
+# update the git repo on develop_R1 branch to include sub-modules
+git checkout develop_R1
+git submodule init
+git submodule update
 
